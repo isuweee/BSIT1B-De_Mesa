c3a932e66a8905f204cf6b7365ad349ae3723dc9
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-07 00:23:43 +0800

    Delete activity2.py

--- activity2.py
+++
@@ -1,7 +0,0 @@
-name = input("Hello, what is your name? ")
-
-<<<<<<< HEAD
-print("Hello " + name + " welcome to my life <3" )
-=======
-print("Hello " + name + " welcome to my life hehe <3" )
->>>>>>> f152ad9 (Update activity2.py with new content)
