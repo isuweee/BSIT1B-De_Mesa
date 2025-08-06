bc2289bcb6387fafd098aa367dded68e1c92b7e5
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-07 00:22:53 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,7 @@
+name = input("Hello, what is your name? ")
+
+<<<<<<< HEAD
+print("Hello " + name + " welcome to my life <3" )
+=======
+print("Hello " + name + " welcome to my life hehe <3" )
+>>>>>>> f152ad9 (Update activity2.py with new content)
