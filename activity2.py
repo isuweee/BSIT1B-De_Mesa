6bd4295858399279ec842cd8e14abd7cabd56c51
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-07 10:56:41 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,11 @@
+name = input("Hello, what is your name? ")
+
+print("Hello " + name + " welcome to my life <3" )
+
+answer = input("Ako pa ba? (yes/no): ")
+if answer == "yes":
+    print("Dapat lang :>")
+elif answer == "no":
+    print("Sakit mo na :<")
+else:
+    print("Pinagsasasabi mo? :|")
