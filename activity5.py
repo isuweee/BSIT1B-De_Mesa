2b6b3be3ad4536ed020db762098a5aaf8236a9aa
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-11 23:44:36 +0800

    eval

---
+++ activity5.py
@@ -0,0 +1,6 @@
+name = eval(input("Enter any character here: "))
+
+print("Your data type is :", type(name))
+
+# i did as teacher taught us, though it won't classify <class str> when i put text on it.
+# if i learn more about it i'll edit this
