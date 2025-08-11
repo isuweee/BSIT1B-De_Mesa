2ab46190dbdaf279da145898e54e4baf659522a8
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-12 00:11:38 +0800

    Code Challenge 1

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+name = input("Enter your full name: ")
+# try my name 'Adrian De Mesa', i modified it to fit my name in the middle.
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t      Hello,\t\t\t*\n\t*\t\t\t ", name,"\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
