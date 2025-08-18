9beeefb1b0270a1fb7144350592a618e00a5f731
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-18 15:14:16 +0800

    assignment operators

---
+++ activity7.py
@@ -0,0 +1,13 @@
+a = 25
+print("The value of 'a' is", a)
+
+a -= 5
+print("The value of 'a' now -5 is ", a)
+
+a += 14
+a *= 2 
+a += 70.
+a /= 2
+a += 0.69
+
+print("The value of 'a' after all the equation is ", a)
