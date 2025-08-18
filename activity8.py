48a5a161fa6dcb4e44240fac468a1eab73327c96
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-18 15:29:58 +0800

    Relational Operators

---
+++ activity8.py
@@ -0,0 +1,23 @@
+a = 5
+b = 10
+c = 15
+d = 20
+e = 10
+
+print("Given a = 5, b = 10, c = 15, d = 20, e = 10")
+
+print("Is b > a? The answer is: ")
+print( b > a)
+
+print("Is d < c? The answer is: ")
+print( d < c )
+
+print("Well, what about b >= e? The answer is: ")
+print( b >= e )
+
+print("Let's try it on names. Given is 'Ace' and 'ace' ")
+name1 = 'Ace'
+name2 = 'ace'
+
+print("Is it not the same? (name1 != name2)? The answer is: ")
+print( name1 != name2 )
