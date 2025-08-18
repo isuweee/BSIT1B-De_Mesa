8e7c19a91c8c4684bbc7725870fd4733dc8ebc6e
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-18 15:47:55 +0800

    Logical Operators

---
+++ activity9.py
@@ -0,0 +1,19 @@
+username = 'isuweee'
+password = 'incorrect'
+
+print((username == 'isuweee') and (password == 'incorrect' ))
+print("Login Successful.")
+
+a = 4
+b = 5
+c = 6
+d = 4
+
+print(( a < b) and (c > d))
+print("Correct :)")
+
+print((a > b) or (c < d))
+print("Incorrect :(")
+
+print(not(( a < b) or (c > d)))
+print("You're right to others, but not on me ;D")
