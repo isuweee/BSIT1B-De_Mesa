543ac4a815fc56ece781baaea65336e38209ac0c
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-18 18:29:18 +0800

    input

--- activity2.py
+++ activity2.py
@@ -1,11 +1,3 @@
 name = input("Hello, what is your name? ")
 
 print("Hello " + name + " welcome to my life <3" )
-
-answer = input("Ako pa ba? (yes/no): ")
-if answer == "yes":
-    print("Dapat lang :>")
-elif answer == "no":
-    print("Sakit mo na :<")
-else:
-    print("Pinagsasasabi mo? :|")
