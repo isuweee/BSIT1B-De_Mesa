9474e7a73a419f0c9211e1dba114e70e50d499a6
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-27 15:03:24 +0800

    conditional statement

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "isuweee"
+password = "passwordkoto"
+
+user = input("Enter your username: ")
+passw = input("Enter your password: ")
+
+if (user == username) and (passw == password):
+    print("Login Successfully!")
+else:
+    print("Incorrect username or password :(")
