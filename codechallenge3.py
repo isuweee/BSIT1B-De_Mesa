fd09ba45283292ba2a574763fe0f3e9ed62e2610
De Mesa, Adrian <acesuueee@gmail.com>
2025-08-27 01:06:06 -0700

    code challenge 3

--- codechallenge3.py
+++ codechallenge3.py
@@ -5,7 +5,7 @@ print("20% discount if you are a student")
 
 student = input("Are you a student? yes or no?: ")
 
-if student.lower() == "yes":
+if student.lower() == 'yes':
     percent = (20 * fare) / 100
     print("You're discounted!")
     print("Your total fee after discount is: ", fare - percent)
