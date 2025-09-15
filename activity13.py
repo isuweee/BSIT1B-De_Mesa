b61aa2bed4c2b45ab456c301d557fab6256288b3
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-15 15:13:30 +0800

    sum automation

---
+++ activity13.py
@@ -0,0 +1,7 @@
+result = 0
+for adrian in range(1,21,1):
+    print(adrian)
+    num = eval(input('Enter a number to sum it all up: '))
+    result += num
+
+print(result, "is the sum of all ur inputs")
