00b905d78f03954c00389c869a2cb0fa48001540
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-17 10:28:34 +0800

    code challenge 7

---
+++ code_challenge7.py
@@ -0,0 +1,9 @@
+print("Liftoff (Countdown Simulator) by De Mesa, Adrian ")
+
+num = eval(input("Put the number u want the countdown to start: "))
+
+for demesa in range(num,0,-1):
+    print(demesa)
+
+print("Liftoff!")
+# It did not exactly countdown per second ;c
