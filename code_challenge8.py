3c08ac52faed3dc76ab16a7a325bbb192e958e10
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-17 10:41:44 +0800

    code challenge 8

--- code_challenge8.py
+++ code_challenge8.py
@@ -2,6 +2,7 @@ print("Multiplication Function by De Mesa, Adrian ;D")
 
 num = eval(input("Enter a number to multiply it to 1-10: "))
 
+print("Multiplication Table for", num, "is: ")
 for x in range(1,11,1):
     result = num * x
     print(num, "x", x, "is", result)
