87ed1dc827176c43293f7c30d7de810199352756
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-17 10:43:42 +0800

    code challenge 9

---
+++ code_challenge9.py
@@ -0,0 +1,8 @@
+print("Parrot(🦜) Simulator - The Echo Chamber (by De Mesa, Adrian)")
+
+squawk = input("What do u want the 🦜  to say?: ")
+num = eval(input("How many times do u want the 🦜  to repeat ur words?: "))
+
+for ace in range(1,num + 1):
+    print("🦜: Squawk! Squawk!", squawk)
+    
