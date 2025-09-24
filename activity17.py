c8e08be6b03a9e258dbd7eb8346bf1ca55f9111a
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-24 12:06:50 +0800

    Nested For Loop (Descending)

---
+++ activity17.py
@@ -0,0 +1,9 @@
+# Nested For Loop(Descending) by De Mesa, Adrian
+
+for i in range(1,11,1):
+    print(i, end=".) ")
+    for x in range(10,0,-1):
+        print(x, end="|")
+    print()
+
+# I made this descending to be different from the example
