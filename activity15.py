ecf3f497795d755945c3b2c5d0fd75d32e32d364
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-24 11:45:38 +0800

    string formatting

--- activity15.py
+++ activity15.py
@@ -5,3 +5,14 @@ ln = 'De Mesa'
 nn = 'Teki'
 
 print(f"My whole name is {fn} {ln}, and my nickname is {nn}")
+
+# String Formatting 'Factorial' version
+
+num = int(input("Enter Number: "))
+result = 1
+
+for i in range(num, 0, -1):
+    print(f"{result} × {i} = {result * i}")
+    result *= i
+
+print(f"Factorial of {num} is {result}")
