ed33ec769f450dd34321b3e860c832996b59fa37
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-29 19:42:45 +0800

    code challenge 12

---
+++ code_challenge12.py
@@ -0,0 +1,10 @@
+# idk what title by De Mesa, Adrian
+
+for i in range(1,6,1):
+    for x in range(5,i,-1):
+        print(' ',end='')
+    for y in range(i,0,-1):
+        print(y,end='')
+    for z in range(2,i+1,1):
+        print(z,end='')
+    print()
