c223f9e6f8d3ce570d2606dc60307ac736a10c63
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-29 15:40:26 +0800

    triangle

---
+++ code_challenge11.py
@@ -0,0 +1,9 @@
+# Triangle by De Mesa, Adrian
+for teki in range(1,11,1):
+    for ace in range(10,teki,-1):
+        print(' ', end='')
+    for adrian in range(1,teki,1):
+        print('x', end='')
+    for demesa in range(1,teki,1):
+        print('x', end='')
+    print()
