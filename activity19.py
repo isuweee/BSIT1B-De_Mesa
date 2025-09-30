77df5692865e9756e5343f677c5231825a84bd6f
De Mesa, Adrian <acesuueee@gmail.com>
2025-09-30 21:39:21 +0800

    kalso daw

---
+++ activity19.py
@@ -0,0 +1,7 @@
+# Kalso daw by De Mesa, Adrian
+for ace in range(1,6,1):
+    for x in range(6,ace,-1):
+        print('y',end='')
+    for y in range(1,ace+1,1):
+        print('x', end='')
+    print()
