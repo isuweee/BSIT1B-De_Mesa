82b0cd84689bdc727e616f822ad835f07d2eb3f7
De Mesa, Adrian <acesuueee@gmail.com>
2025-10-15 15:51:17 +0800

    code challenge 13

--- code_challenge13.py
+++ code_challenge13.py
@@ -1,21 +1,25 @@
-# Odd Number Summation While Loop by De Mesa, Adrian ^^
-
-print(f"========================\n  ODD NUMBER SUMMATION\n========================\n")
+print(f"========================\n  ODD/EVEN SUMMATION\n========================\n")
 
 name = input("Enter your name: ")
 odd = 0
 num_name = ""
+even = 0
+even_name = ""
 
 while True:
     num = eval(input("Enter a random number: "))
     if num == 0:
         print(f"Program STOPS!\nHello {name}, the total of ODD is: {odd}\nThe ODD components are: {num_name}")
+        print(f"The total of EVEN is: {even}\nThe EVEN components are: {even_name}")
         break
     elif num % 2 == 1:
         print("This is an ODD number!")
         odd += num
         num_name += str(num) + " "
         continue
-    elif num % 2 == 0:
-        print("This is an EVEN number!")
+    else:
+        if num % 2 == 0:
+            print("This is an EVEN number!")
+            even += num
+            even_name += str(num) + " "
         continue
